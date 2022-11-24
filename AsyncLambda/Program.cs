2e2fb2a621d39940f7bc548cdda41bfa8abf78c1
abdujalilc <abdujalilc@outlook.com>
2022-11-24 17:29:30 +0500

    folder replace

---
+++ AsyncLambda/Program.cs
@@ -0,0 +1,11 @@
+﻿// асинхронное лямбда-выражение
+Func<string, Task> printer = async (message) =>
+{
+    await Task.Delay(3000);
+    Console.WriteLine(message);
+};
+
+await printer("Hello World");
+await printer("Hello METANIT.COM");
+
+Console.ReadLine();
---
+++ TaskDelay/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+await PrintAsync();   // вызов асинхронного метода
+Console.WriteLine("Некоторые действия в методе Main");
+Console.ReadLine();
+// определение асинхронного метода
+async Task PrintAsync()
+{
+    await Task.Delay(3000);     // имитация продолжительной работы
+    // или так
+    //await Task.Delay(TimeSpan.FromMilliseconds(3000));
+    Console.WriteLine("Hello METANIT.COM");
+}
+
+PrintName("Tom");
+PrintName("Bob");
+PrintName("Sam");
+
+void PrintName(string name)
+{
+    Thread.Sleep(3000);     // имитация продолжительной работы
+    Console.WriteLine(name);
+}
+
+Console.ReadLine();
+
+await PrintNameAsync("Tom");
+await PrintNameAsync("Bob");
+await PrintNameAsync("Sam");
+
+// определение асинхронного метода
+async Task PrintNameAsync(string name)
+{
+    await Task.Delay(3000);     // имитация продолжительной работы
+    Console.WriteLine(name);
+}
+
+Console.ReadLine();
