9b32b1eb8d5ced212648126d31909b3c0c3e1dc7
abdujalilc <abdujalilc@outlook.com>
2023-02-02 18:35:24 +0500

    cancellation token added

---
+++ CancellationToken/Program.cs
@@ -0,0 +1,25 @@
+﻿CancellationTokenSource? tokenSource = new CancellationTokenSource();
+
+CancellableMethod();
+
+if (Console.ReadKey().KeyChar is 'c' or 'C')
+{
+    Console.WriteLine("\n" + "Thread is about to cancel");
+    Thread.Sleep(3000);
+    tokenSource.Cancel();
+}
+
+void CancellableMethod()
+{
+    Task.Run(() =>
+    {
+        int maxIterations = 100;
+        for (int i = 0; i < maxIterations; ++i)
+        {
+            Console.WriteLine(i);
+            Thread.Sleep(1000);
+            if (tokenSource.IsCancellationRequested)
+                tokenSource.Token.ThrowIfCancellationRequested();
+        }
+    }, tokenSource.Token);
+}
